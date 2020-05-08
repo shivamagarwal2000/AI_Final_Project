72011f33ac4e3307e87fb7ab4558e6c642e85bc9
Shivam Agarwal <shivam2003agarwal@gmail.com>
2020-05-08 14:08:39 +1000

    terminal function

--- 2020-part-B-skeleton-1_1/DeepMagic/evaluation.py
+++ 2020-part-B-skeleton-1_1/DeepMagic/evaluation.py
@@ -2,20 +2,31 @@
 # Eval(s) = w1f1(s) + w2f2(s) + . . . + wnfn(s)
 
 
-
 def get_no_pieces(game_state, type):
-    ans  = 0
+    ans = 0
     for i in range(0, 8):
         for j in range(0, 8):
             if game_state.x == i and game_state.y == j:
-                if game_state.piece_type == type :
+                if game_state.piece_type == type:
                     ans += game_state.no_of_pieces
 
     return ans
+
 
 def eval(game_state):
     frnd_pieces = get_no_pieces(game_state, frnd)
     enem_pieces = get_no_pieces(game_state, enem)
     weight = 10
 
-    return weight*(frnd_pieces - enem_pieces)
+    return weight * (frnd_pieces - enem_pieces)
+
+
+def utility(game_state):
+    enem_pieces = get_no_pieces(game_state, enem)
+    frnd_pieces = get_no_pieces(game_state, frnd)
+    if enem_pieces == 0:
+        return 1000
+    elif frnd_pieces == 0:
+        return -1000
+    else:
+        return 0
